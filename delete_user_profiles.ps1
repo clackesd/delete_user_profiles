b67ed0dcf97c5c2eb6be593cd575fd22d274dd76
Tim Gross <TGross@clackesd.k12.or.us>
2024-04-30 10:51:02 -0700

    Got it detecting logged on or not

--- delete_user_profiles.ps1
+++ delete_user_profiles.ps1
@@ -4,26 +4,29 @@ $targetOU = "OU=Districts,DC=apps,DC=clackesd,DC=k12,DC=or,DC=us"
 # Import the Active Directory module
 Import-Module ActiveDirectory
 
-# Get a list of user profiles below the specified OU
 $profiles = Get-WmiObject Win32_UserProfile | Where-Object { $_.Special -eq $false -and $_.LocalPath -like "C:\Users\*" } | ForEach-Object {
     $profileSID = $_.SID
     $user = Get-ADUser -Filter { SID -eq $profileSID } -Properties DistinguishedName
     if ($user.DistinguishedName -like "*,$targetOU") {
         $_
+        }
     }
-}
+
 # Iterate through each profile below the specified OU
 
 foreach ($profile in $profiles) {
     # Check if the user is logged in
-    $loggedIn = quser /server:YourTerminalServer | Where-Object { $_ -match $profile.LocalPath.Split('\')[1] }
+
+    $loggedIn = quser /server:localhost | Where-Object { $_ -match $profile.LocalPath.Split('\')[2] }
+    #Write-Host "Logged in: $($loggedIn)"
 
     # If the user is not logged in, delete the profile
+    
     if (-not $loggedIn) {
-        Write-Host "Deleting profile: $($profile.LocalPath)"
+        Write-Host "Would be deleting profile: $($profile.LocalPath)"
         #Remove-WmiObject -InputObject $profile -WhatIf # Use -WhatIf to simulate, remove it to perform actual deletion
         }
     else {
         Write-Host "Not Deleting profile: $($profile.LocalPath) logged in!"
         }
-
+    }
